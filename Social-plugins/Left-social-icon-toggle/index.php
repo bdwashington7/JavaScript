c08ecc25fddc5d174b8619ff80e6e7c5ea476511
Shah Ali Bogdadi <bogdadishahali@gmail.com>
2015-11-11 11:52:04 +0600

    Uploaded 3 JS Folders
    Social-left-toggle
    JS-Form-Password-Check
    JS-Form-Password-Submit-Check

---
+++ Social-plugins/Left-social-icon-toggle/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>JS Form Validation</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css" />
+	<style type="text/css">
+		body{
+			width: 970px;
+			margin: 50px auto;
+		}
+	</style>
+</head>
+<body>
+<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-563df380edbe40ad" async="async"></script>
+	<h3>Output :</h3>
+	<img src="social-toggle.png" alt="Image" width="500px">
+	<p>This plugin will run from server. Please run your <mark>localhost</mark> to see the output...</p>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</body>
+</html>
